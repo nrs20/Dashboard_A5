4e91d71c1db3f3ff4305f436498f0b8d3d883367
nrs20 <nrs5@njit.edu>
2024-03-30 23:52:13 -0400

    testing push

---
+++ AndroidStudioProjects/388GroupProj/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "388-Group-Proj"
+include(":app")
+ 
